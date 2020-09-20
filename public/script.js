75bc44eb7525b531d8af2e782163e76e378c4110
syamjithas <syamjithas@gmail.com>
2020-09-20 22:15:59 +0400

    peerjspath

--- public/script.js
+++ public/script.js
@@ -1,8 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+  host: '/peerjs',
 })
 const myVideo = document.createElement('video')
 myVideo.id="my-video"
